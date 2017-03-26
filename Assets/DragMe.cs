24ea9f72ca449f1147dbc0d72c93a678e18f9116
omikun <omikun@gmail.com>
2017-03-25 22:09:35 -0700

    initial commit
    textures found online; can drag a card around

---
+++ Assets/DragMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+[RequireComponent(typeof(SphereCollider))]
+ 
+public class DragMe : MonoBehaviour
+{
+    void OnMouseDrag()
+    {
+		Debug.Log("on mouse drag");
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point.z = gameObject.transform.position.z;
+        gameObject.transform.position = point;
+        Cursor.visible = false;
+    }
+   
+    void OnMouseUp()
+    {
+		Debug.Log("on mouse up");
+        Cursor.visible = true;
+    }
+}
