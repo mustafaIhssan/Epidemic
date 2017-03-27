39c664e94c82e5314f849869de031d40349eb0b1
omikun <omikun@gmail.com>
2017-03-26 18:21:34 -0700

    Implemented graph of cities
    cities are clickable, can identify neighbors

---
+++ Assets/CityGraph.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+	public Node (GameObject o) {
+		obj = o;
+		links = new List<Node>();
+	}
+	public List<Node> GetNeighbors() {
+		return links;
+	}
+	static int addCount = 0;
+	public void AddOneWay(Node newNode) {
+        //if not already there?
+        if (newNode == null)
+		{
+			Debug.Log(obj.name + " trying to add invalid node given");
+		}
+		//Debug.Log(obj.name + "->" + newNode.obj.name);
+		if (!links.Contains(newNode))
+            links.Add(newNode);
+	}
+	public void Add(Node newNode) {
+		if (addCount++ > 50) 
+			return;
+
+        AddOneWay(newNode);
+		//Debug.Log("reverse link: " + newNode.obj.name + "->" + obj.name );
+		newNode.AddOneWay(this);
+	}
+	public GameObject GetObj() { return obj; }
+	List<Node> links;
+	GameObject obj;
+}
+public class CityGraph : MonoBehaviour {
+	Dictionary<string, Node> cities = new Dictionary<string, Node>();
+	Node dummy;
+	// Use this for initialization
+	void Start () {
+		dummy = new Node(transform.gameObject);
+        //get list of all cities
+		Debug.Log("Cities has " + transform.GetChildCount());
+        foreach (Transform tChild in transform) {
+			var child = tChild.gameObject;
+			if (child == null)
+			{
+				Debug.Log("empty child");
+				continue;
+			}
+			cities.Add(child.name, new Node(child));
+			child.tag = "NoDrag";
+		}
+		InitGraph();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public List<Node> GetNeighbors(GameObject input) {
+		var node = cities[input.name];
+		return node.GetNeighbors();
+
+	}
+	void InitGraph() {
+		
+
+        dummy.AddOneWay(cities["Atlanta"]);
+		cities["Atlanta"].Add(cities["Chicago"]);
+		cities["Atlanta"].Add(cities["Washington"]);
+		cities["Atlanta"].Add(cities["Miami"]);
+		
+		cities["Chicago"].Add(cities["Montreal"]);
+		cities["Chicago"].Add(cities["San Francisco"]);
+		cities["Chicago"].Add(cities["Los Angeles"]);
+		cities["Chicago"].Add(cities["Mexico City"]);
+
+		cities["Montreal"].Add(cities["New York"]);
+		cities["Montreal"].Add(cities["Washington"]);
+
+		cities["Washington"].Add(cities["New York"]);
+		cities["Washington"].Add(cities["Miami"]);
+
+		cities["New York"].Add(cities["London"]);
+		cities["New York"].Add(cities["Madrid"]);
+
+		cities["San Francisco"].Add(cities["Tokyo"]);
+		cities["San Francisco"].Add(cities["Manila"]);
+		cities["San Francisco"].Add(cities["Los Angeles"]);
+
+		cities["Los Angeles"].Add(cities["Mexico City"]);
+		cities["Los Angeles"].Add(cities["Sydney"]);
+		cities["Los Angeles"].Add(cities["Lima"]);
+
+		cities["Mexico City"].Add(cities["Bogota"]);
+		cities["Mexico City"].Add(cities["Lima"]);
+		cities["Mexico City"].Add(cities["Miami"]);
+
+		cities["Bogota"].Add(cities["Lima"]);
+		cities["Bogota"].Add(cities["Buenos Aires"]);
+		cities["Bogota"].Add(cities["Sao Paulo"]);
+
+		cities["Lima"].Add(cities["Santiago"]);
+
+		cities["Santiago"].Add(cities["Buenos Aires"]);
+
+		cities["Buenos Aires"].Add(cities["Sao Paulo"]);
+		cities["Buenos Aires"].Add(cities["Johannesburg"]);
+
+		cities["Lagos"].Add(cities["Lagos"]);
+		cities["Lagos"].Add(cities["Madrid"]);
+
+		cities["Kinshasa"].Add(cities["Khartoum"]);
+		cities["Kinshasa"].Add(cities["Johannesburg"]);
+
+		cities["Khartoum"].Add(cities["Johannesburg"]);
+		cities["Khartoum"].Add(cities["Cairo"]);
+
+		cities["Cairo"].Add(cities["Algiers"]);
+		cities["Cairo"].Add(cities["Istanbul"]);
+		cities["Cairo"].Add(cities["Baghdad"]);
+		cities["Cairo"].Add(cities["Riyadh"]);
+
+		cities["Algiers"].Add(cities["Madrid"]);
+		cities["Algiers"].Add(cities["Paris"]);
+		cities["Algiers"].Add(cities["Istanbul"]);
+
+		cities["Istanbul"].Add(cities["Milan"]);
+		cities["Istanbul"].Add(cities["St. Petersburg"]);
+		cities["Istanbul"].Add(cities["Moscow"]);
+		cities["Istanbul"].Add(cities["Baghdad"]);
+
+		cities["Baghdad"].Add(cities["Tehran"]);
+		cities["Baghdad"].Add(cities["Riyadh"]);
+
+		cities["London"].Add(cities["Madrid"]);
+		cities["London"].Add(cities["Paris"]);
+		cities["London"].Add(cities["Essen"]);
+		
+		cities["Madrid"].Add(cities["Paris"]);
+		
+		cities["Paris"].Add(cities["Essen"]);
+		cities["Paris"].Add(cities["Milan"]);
+
+		cities["Essen"].Add(cities["Milan"]);
+		cities["Essen"].Add(cities["St. Petersburg"]);
+
+		cities["Moscow"].Add(cities["St. Petersburg"]);
+		cities["Moscow"].Add(cities["Tehran"]);
+
+		cities["Tehran"].Add(cities["Delhi"]);
+		cities["Tehran"].Add(cities["Karachi"]);
+
+		cities["Karachi"].Add(cities["Delhi"]);
+		cities["Karachi"].Add(cities["Riyadh"]);
+		cities["Karachi"].Add(cities["Mumbai"]);
+
+		cities["Delhi"].Add(cities["Mumbai"]);
+		cities["Delhi"].Add(cities["Chennai"]);
+		cities["Delhi"].Add(cities["Kolkata"]);
+
+		cities["Chennai"].Add(cities["Kolkata"]);
+		cities["Chennai"].Add(cities["Mumbai"]);
+		cities["Chennai"].Add(cities["Jakarta"]);
+
+		cities["Bangkok"].Add(cities["Jakarta"]);
+		cities["Bangkok"].Add(cities["Kolkata"]);
+		cities["Bangkok"].Add(cities["Hong Kong"]);
+		cities["Bangkok"].Add(cities["Ho Chi Minh City"]);
+
+		cities["Hong Kong"].Add(cities["Ho Chi Minh City"]);
+		cities["Hong Kong"].Add(cities["Kolkata"]);
+		cities["Hong Kong"].Add(cities["Shanghai"]);
+		cities["Hong Kong"].Add(cities["Taipei"]);
+		cities["Hong Kong"].Add(cities["Manila"]);
+
+		cities["Shanghai"].Add(cities["Taipei"]);
+		cities["Shanghai"].Add(cities["Beijing"]);
+		cities["Shanghai"].Add(cities["Seoul"]);
+		cities["Shanghai"].Add(cities["Tokyo"]);
+
+		cities["Seoul"].Add(cities["Tokyo"]);
+		cities["Seoul"].Add(cities["Beijing"]);
+
+		cities["Osaka"].Add(cities["Tokyo"]);
+		cities["Osaka"].Add(cities["Taipei"]);
+
+		cities["Jakarta"].Add(cities["Ho Chi Minh City"]);
+		cities["Jakarta"].Add(cities["Sydney"]);
+
+		cities["Manila"].Add(cities["Ho Chi Minh City"]);
+		cities["Manila"].Add(cities["Sydney"]);
+	}
+}
--- Assets/Common.cs
+++ Assets/Common.cs
@@ -13,132 +13,19 @@ public static class SortingOrder {
 
 }
 
-public static class Cities {
-	public static List<List<string>> GetCityLists() {
-		List<List<string>> types = new List<List<string>>();
-		types.Add(blue);
-		types.Add(red);
-		types.Add(yellow);
-		types.Add(black);
-		
-		return types;
-	}
-	static void Shuffle(ref List<string> deck) {
-		for (int i = 0; i < deck.Count; i++)
-        {
-            string temp = deck[i];
-            int randomIndex = UnityEngine.Random.Range(i, deck.Count);
-            deck[i] = deck[randomIndex];
-            deck[randomIndex] = temp;
-        }
-	}
-	public static List<string> GetDeck() {
-		var types = GetCityLists();
-		//get all cities in one list
-		List<string> cities = new List<string>();
-		cities.AddRange(blue);
-		cities.AddRange(red);
-		cities.AddRange(yellow);
-		cities.AddRange(black);
 
-		for (int i = 0; i < 17; i++)
-            Shuffle(ref cities);
-#if true
-        foreach (var city in cities) {
-			var type = GetType(city);
-			Debug.Log(type + ": " + city);
-		}
-#endif
-        return cities;
-	}
-	public static string GetType(string c) {
-		List<string> typeName = new List<string>(new string[] { 
-			"blue", 
-			"red", 
-			"yellow",
-			"black"
-		});
-
-		var types = GetCityLists();
-		int i = 0;
-		foreach(var type in types) {
-			foreach(var city in type) {
-				if (city.Equals(c, StringComparison.Ordinal)) {
-					return typeName[i];
-				}
-			}
-			i++;
-		}
-		Debug.Log("not a valid city given!");
-		return "invalid";
-	}
-	public static List<string> blue = new List<string>(new string[] { 
-		"San Francisco",
-		"Chicago",
-		"Atlanta",
-		"Montreal",
-		"New York",
-		"Washtingon",
-		"London",
-		"Madrid",
-		"Paris",
-		"Essen",
-		"Milan",
-		"St. Petersburg"
-	});
-	public static List<string> yellow = new List<string>(new string[] { 
-		"Lagos",
-		"Khartoum",
-		"Kinshasa",
-		"Johannesburg",
-		"Sao Paulo",
-		"Bueno Aires",
-		"Santiago",
-		"Lima",
-		"Bogota",
-		"Miami",
-		"Mexico City",
-		"Los Angeles"
-	});
-	public static List<string> black = new List<string>(new string[] { 
-		"Algiers",
-		"Istanbul",
-		"Cairo",
-		"Baghdad",
-		"Moscow",
-		"Tehran",
-		"Karachi",
-		"Delhi",
-		"Kolkata",
-		"Mumbai",
-		"Chennai"
-	});
-
-	public static List<string> red = new List<string>(new string[] { 
-		"Bangkok",
-		"Beijing",
-		"Shanghai",
-		"Hong Kong",
-		"Seoul",
-		"Tokyo",
-		"Osaka",
-		"Taipei",
-		"Manila",
-		"Ho Chi Minh City",
-		"Jakarta",
-		"Sydney"
-	});
-
-
-}
 public class Common : MonoBehaviour {
 
     Vector3 offset;
     bool firstClicked;
 	GameObject mouseSelection;
+	CityGraph cg;
 	// Use this for initialization
 	void Start () {
         firstClicked = true;
+		cg = GameObject.Find("Cities").GetComponent<CityGraph>();
+		if (cg == null)
+			Debug.Log("Can't find citygraph!?!");
 	}
 	
 	// Update is called once per frame
@@ -162,6 +49,14 @@ public class Common : MonoBehaviour {
 				if (deck != null)
 				{
 					deck.Draw();
+				}
+				if (mouseSelection.transform.parent.name == "Cities")
+				{
+					Debug.Log("clicked on city: " + mouseSelection.name);
+					var neighbors = cg.GetNeighbors(mouseSelection);
+					foreach (var node in neighbors) {
+						Debug.Log("Neighbor: " + node.GetObj().name);
+					}
 				}
 			}
         } else if (Input.GetMouseButton(0)) 
